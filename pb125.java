06219208d69a7404da21cb37e73f8c77d222e86d
JaeMin <46991162+jaemin2682@users.noreply.github.com>
2020-01-21 11:27:27 +0900

    반복제어문1 - 형성평가1

---
+++ pb125.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pb125 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int num = s.nextInt();
+		for(int i=1;i<=num;i++) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
